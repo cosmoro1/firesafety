d19b7a89464d4be63761ba61c72d3ed44a1fc303
chavezhoneyjean23-d <chavezhoneyjean23@gmail.com>
2026-01-30 23:19:15 +0800

    linked user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents; // You can remove this if unused
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // This command runs your other file!
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
